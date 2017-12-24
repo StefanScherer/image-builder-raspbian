88d3f07d55ba579e7209d0cf29eae17a4ae42a15
Stefan Scherer <scherer_stefan@icloud.com>
2017-12-24 10:26:18 +0100

    Add user-data file

--- stage2/02-cloud-init/01-run.sh
+++ stage2/02-cloud-init/01-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+install -m 644 files/user-data ${ROOTFS_DIR}/boot/
+install -m 644 files/meta-data ${ROOTFS_DIR}/boot/
+
 on_chroot << EOF
 echo 'Symlinking cloud-init nocloud-net seed files to /boot partition'
 mkdir -p /var/lib/cloud/seed/nocloud-net
