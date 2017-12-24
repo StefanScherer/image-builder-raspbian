ee4d065a743257e14810197e3fc2ac9fbd20d790
Stefan Scherer <scherer_stefan@icloud.com>
2017-12-24 10:36:06 +0100

    Skip stage5

--- build.sh
+++ build.sh
@@ -16,8 +16,11 @@ git checkout "$PI_GEN_TAG"
 echo "Preparing build"
 echo IMG_NAME='hypriotos' >config
 cp -r ../stage2/ stage2
-touch ./stage3/SKIP ./stage4/SKIP
-rm stage4/EXPORT*
+touch stage3/SKIP
+touch stage4/SKIP
+rm -f stage4/EXPORT*
+touch stage5/SKIP
+rm -f stage5/EXPORT*
 
 echo "Build image"
 ./build-docker.sh
