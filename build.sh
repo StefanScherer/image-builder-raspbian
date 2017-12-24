707d888308dd27cd1873fa4e61053eec5eeeb2f1
Stefan Scherer <scherer_stefan@icloud.com>
2017-12-24 10:11:51 +0100

    Build image

--- build.sh
+++ build.sh
@@ -12,3 +12,13 @@ git clone "https://github.com/$PI_GEN_REPO" pi-gen
 cd pi-gen
 echo "Checkout $PI_GEN_TAG"
 git checkout "$PI_GEN_TAG"
+
+echo "Preparing build"
+echo IMG_NAME='hypriotos' >config
+cp -r ../stage2/ stage2
+touch ./stage3/SKIP ./stage4/SKIP
+rm stage4/EXPORT*
+
+echo "Build image"
+./build-docker.sh
+ls -l deploy
