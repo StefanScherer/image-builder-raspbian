be01e89df682dc3d5be15131a509feb4c237a5e6
Stefan Scherer <scherer_stefan@icloud.com>
2017-12-31 17:37:48 +0100

    Use CIRCLE_TAG

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-export TAG=$(git describe --exact-match "$CIRCLE_SHA1" 2>/dev/null)
-if [ "${CIRCLE_BRANCH}" == "master" -a -n "${TAG}" ]; then
+echo "$CIRCLE_TAG"
+
+if [ "$CIRCLE_TAG" != "" ]; then
   go get github.com/tcnksm/ghr
   mkdir dist
   cp pi-gen/deploy/*.zip dist
@@ -9,7 +10,7 @@ if [ "${CIRCLE_BRANCH}" == "master" -a -n "${TAG}" ]; then
   ZIP_FILE=$(ls -1 *-hypriotos-lite.zip | tail -1)
   sha256sum "${ZIP_FILE}" > "${ZIP_FILE}.sha256"
   popd
-  ghr --username StefanScherer --token $GITHUB_TOKEN --replace "${TAG}" dist/
+  ghr --username StefanScherer --token $GITHUB_TOKEN --replace "${CIRCLE_TAG}" dist/
 else
   echo "No release tag detected. Skip deployment."
 fi
