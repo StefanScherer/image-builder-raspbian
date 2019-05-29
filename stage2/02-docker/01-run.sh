77bb1bb5b0cbfaed31757cba934fcb8004888f9b
kraeml <kraeml@sesamestreet.franken.de>
2019-05-29 15:42:51 +0200

    Remove docker-compose because not working

--- stage2/02-docker/01-run.sh
+++ stage2/02-docker/01-run.sh
@@ -7,6 +7,9 @@ then
 	curl -SsL https://get.docker.com | sh
 fi
 
+#echo 'Installing Docker Compose'
+#pip install docker-compose
+
 # set default locales to 'en_US.UTF-8'
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
