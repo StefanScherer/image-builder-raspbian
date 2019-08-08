03803f17ccc9ea7ddab040280e5c81609f33dc08
kraeml <kraeml@sesamestreet.franken.de>
2019-08-08 14:34:56 +0200

    Refactor build into docker and bash skript.

--- build-docker.sh
+++ build-docker.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
-set -e
-
 scriptpath=$(cd $(dirname $0); pwd -P)
 source "$scriptpath/versions.config"
+VAGRANT_HOME=/home/vagrant
+PI_GEN=$VAGRANT_HOME/pi-gen-docker
+#Load pi-gen repro and change into it
+source $scriptpath/build-source.sh
 
-echo "Cloning $PI_GEN_REPO"
-if [ -d pi-gen ]
-then
-  rm -rf pi-gen
-fi
-git clone "https://github.com/$PI_GEN_REPO" pi-gen
-cd pi-gen
-if [ ! -z "$PI_GEN_TAG" ]; then
-  echo "Checkout $PI_GEN_TAG"
-  git checkout "$PI_GEN_TAG"
-fi
-
-echo "Preparing build"
-echo IMG_NAME='hypriotos-devops-acadamy' >config
-# e.g. LOCAL_APT_PROXY="http://YOUR_FQDN:3142" ./build.sh
-if [ ! -z "$LOCAL_APT_PROXY" ]
-then
-  echo "APT_PROXY=$LOCAL_APT_PROXY" >> config
-fi
-if [ ! -z "$LOCAL_PIP_PROXY" ]
-then
-  echo "PIP_PROXY=$LOCAL_PIP_PROXY" >> config
-fi
-echo "FIRST_USER_NAME=pirate" >> config
-echo "FIRST_USER_PASS=hypriot" >> config
-echo "ENABLE_SSH=1" >> config
-#echo 'STAGE_LIST="stage0 stage1 stage2"' >> config
-cp -r ../stage2/ .
-#touch stage3/SKIP
-#touch stage4/SKIP
-#touch stage4/SKIP_IMAGES
-#rm -f stage4/EXPORT*
-#touch stage5/SKIP
-#touch stage5/SKIP_IMAGES
-#rm -f stage5/EXPORT*
+sed -i 's/FROM debian:stretch/FROM i386\/debian:stretch/g' $PI_GEN/Dockerfile
 
 echo "Build image"
 if [[ $(docker ps \
@@ -51,3 +19,8 @@ else
   ./build-docker.sh
 fi
 ls -l deploy
+cp --recursive --backup deploy $scriptpath/
+if [ -d $VAGRANT_HOME/apt-cacher-ng ]
+then
+  rsync -av --progress --delete $VAGRANT_HOME/apt-cacher-ng /vagrant/
+fi
---
+++ build-source.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+if [[ ! $(docker ps \
+        --all \
+        --filter "name=^vagrant_apt-cacher-ng" \
+        --format '{{.Names}}' ) == vagrant_apt-cacher-ng_1 ]]
+then
+  docker-compose --file $VAGRANT_HOME/docker-compose.yml up -d
+fi
+
+echo "Cloning $PI_GEN_REPO"
+if [ -d ${PI_GEN} ]
+then
+  sudo rm -rf ${PI_GEN}
+fi
+git clone "https://github.com/$PI_GEN_REPO" ${PI_GEN}
+cd ${PI_GEN}
+if [ ! -z "$PI_GEN_TAG" ]; then
+  echo "Checkout $PI_GEN_TAG"
+  git checkout "$PI_GEN_TAG"
+fi
+
+echo "Preparing build"
+echo IMG_NAME='hypriotos-devops-acadamy' >config
+# e.g. LOCAL_APT_PROXY="http://YOUR_FQDN:3142" ./build.sh
+if [ ! -z "$LOCAL_APT_PROXY" ]
+then
+  echo "APT_PROXY=$LOCAL_APT_PROXY" >> config
+fi
+if [ ! -z "$LOCAL_PIP_PROXY" ]
+then
+  echo "PIP_PROXY=$LOCAL_PIP_PROXY" >> config
+fi
+#echo "FIRST_USER_NAME=pirate" >> config
+#echo "FIRST_USER_PASS=hypriot" >> config
+echo "ENABLE_SSH=1" >> config
+#echo 'STAGE_LIST="stage0 stage1 stage2"' >> config
+cp -r $scriptpath/stage2/ .
+#touch stage3/SKIP
+#touch stage4/SKIP
+#touch stage4/SKIP_IMAGES
+#rm -f stage4/EXPORT*
+#touch stage5/SKIP
+#touch stage5/SKIP_IMAGES
+#rm -f stage5/EXPORT*
