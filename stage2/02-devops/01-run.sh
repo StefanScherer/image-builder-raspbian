f7c82836d8c3b36cd63dc7dab2cdc46f42b7438a
kraeml <kraeml@sesamestreet.franken.de>
2019-09-24 22:27:48 +0200

    Missing packages into devops

--- stage2/02-devops/01-run.sh
+++ stage2/02-devops/01-run.sh
@@ -11,9 +11,13 @@ echo 'Get selenium'
 pip3 install selenium \
   pyvirtualdisplay
 
-su --login --command 'git clone -b master https://kraeml@gitlab.com/kraeml/raspberry-edu-devops.git ~/.raspberry-edu-devops || (cd ~/.raspberry-edu-devops; git pull)' pi
-su --login --command 'cd ~/.raspberry-edu-devops/; mkdir roles || true; ansible-galaxy install --force --role-file=requirements.yml' pi
-#su --login --command 'ansible-playbook --inventory ~/.raspberry-edu-devops/hosts --limit lokal ~/.raspberry-edu-devops/site.yml' pi
+echo 'Get gems'
+gem install --no-document bundler inspec:'1.51.25'
+
+
+su --login --command 'git clone -b master https://kraeml@gitlab.com/kraeml/raspberry-edu-devops.git ~/.raspberry-edu-devops || (cd ~/.raspberry-edu-devops; git pull)' ${FIRST_USER_NAME}
+su --login --command 'cd ~/.raspberry-edu-devops/; mkdir roles || true; ansible-galaxy install --force --role-file=requirements.yml' ${FIRST_USER_NAME}
+#su --login --command 'ansible-playbook --inventory ~/.raspberry-edu-devops/hosts --limit lokal ~/.raspberry-edu-devops/site.yml' ${FIRST_USER_NAME}
 #TODO not running because wrong hostname
-#su --login --command 'ansible-playbook --inventory ~/.raspberry-edu-devops/hosts --limit $(hostname) ~/.raspberry-edu-devops/site.yml' pi
+#su --login --command 'ansible-playbook --inventory ~/.raspberry-edu-devops/hosts --limit $(hostname) ~/.raspberry-edu-devops/site.yml' ${FIRST_USER_NAME}
 EOF
